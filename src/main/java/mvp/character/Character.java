030a60c5a23d19850d2887cce5736315f1877110
Arnaud <julian.arnaud.z10@gmail.com>
2016-02-05 14:17:18 +0100

    First commit

---
+++ src/main/java/mvp/character/Character.java
@@ -0,0 +1,8 @@
+package mvp.character;
+
+/**
+ * @author Julian ARNAUD
+ * @date 05/02/2016
+ **/
+public class Character {
+}
---
+++ src/main/java/mvp/stuff/Item.java
@@ -0,0 +1,8 @@
+package mvp.stuff;
+
+/**
+ * @author Julian ARNAUD
+ * @date 05/02/2016
+ **/
+public class Item {
+}
