5ae6c3e6edcbc70ac05291fe8590047d5c6ca5ea
Arnaud <julian.arnaud.z10@gmail.com>
2016-03-10 23:18:38 +0100

    Debut creation d'un donjon + ajout de méthodes pour linker room entre elles

--- src/main/java/mvp/room/Dungeon.java
+++ src/main/java/mvp/room/Dungeon.java
@@ -1,8 +1,32 @@
 package mvp.room;
 
+import mvp.character.Character;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * @author Julian ARNAUD
  * @date 08/03/2016
  **/
 public class Dungeon {
+    private ArrayList<Room> roomList;
+
+    private Location initialLocation;
+
+    private Character hero;
+
+    private boolean done;
+
+    /**
+     * Basic dungeon, 10 rooms, all in a straight line
+     */
+    public Dungeon(Character character){
+        this.done = false;
+        this.hero = character;
+        this.initialLocation = new Location(1,1);
+        this.roomList = new ArrayList<>();
+        Room room10 = new Room(10, new Location(1,10));
+        Room room9 = new Room(9, new Location(1,9));
+    }
 }
--- src/main/java/mvp/room/Room.java
+++ src/main/java/mvp/room/Room.java
@@ -12,16 +12,58 @@ public class Room {
     private Location location;
     private String description;
 
-    private HashMap<Direction, Room> directions;
+    private Room northRoom;
+    private Room eastRoom;
+    private Room southRoom;
+    private Room westRoom;
 
-    public Room(int id, Location location, HashMap<Direction, Room> directions){
+    public Room(int id, Location location){
         this.id = id;
         this.location = location;
-        this.directions = directions;
         this.description = "Room n°: " + id + ", location: (" + location.getX() + "," + location.getY() + ")";
+        northRoom = null;
+        eastRoom = null;
+        southRoom = null;
+        westRoom = null;
     }
 
     public String getDescription(){
         return this.description;
+    }
+
+    public void linkNorth(Room room){
+        northRoom = room;
+        room.setSouthRoom(this);
+    }
+
+    public void linkSouth(Room room){
+        southRoom = room;
+        room.setNorthRoom(this);
+    }
+
+    public void linkEast(Room room){
+        eastRoom = room;
+        room.setWestRoom(this);
+    }
+
+    public void linkWest(Room room){
+        westRoom = room;
+        room.setEastRoom(this);
+    }
+
+    public void setEastRoom(Room eastRoom) {
+        this.eastRoom = eastRoom;
+    }
+
+    public void setNorthRoom(Room northRoom) {
+        this.northRoom = northRoom;
+    }
+
+    public void setSouthRoom(Room southRoom) {
+        this.southRoom = southRoom;
+    }
+
+    public void setWestRoom(Room westRoom) {
+        this.westRoom = westRoom;
     }
 }
